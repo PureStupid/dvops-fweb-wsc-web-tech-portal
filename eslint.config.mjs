9e697bce49adbcd9c29f5a8b85db8ad1a6397a62
Pure <111236541+PureStupid@users.noreply.github.com>
2024-10-22 11:04:40 +0800

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginReact.configs.flat.recommended,
+    eslintPluginReact.configs.flat['jsx-runtime'],
+    eslintPluginPrettierRecommended,
+    includeIgnoreFile(gitignorePath),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        plugins: {
+            'react-hooks': eslintPluginReactHooks,
+        },
+
+        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+
+        rules: {
+            ...eslintPluginReactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react/no-unescaped-entities': 'off',
+        },
+    },
+);
