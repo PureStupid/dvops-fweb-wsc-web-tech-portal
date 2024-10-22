08a68f13a092367340a72833ef209140924a16d9
KohKeira <kohkeira996@gmail.com>
2024-10-22 19:19:33 +0800

    Remove navigation bar for Welcom page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import SecondaryButton from '@/Components/SecondaryButton';
 import { PageProps } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 
@@ -12,41 +13,30 @@ export default function Welcome({
             <div className="bg-[url('/images/background.jpg')] text-black dark:text-white/50">
                 <div className="relative flex min-h-screen flex-col items-center selection:bg-[#FF2D20] selection:text-white">
                     <div className="relative flex w-full max-w-2xl grow flex-col px-6 lg:max-w-7xl">
-                        <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                            <div className="flex lg:col-start-2 lg:justify-center">
-                                <img
-                                    src="/images/logo.png"
-                                    alt="WSC logo"
-                                    height={100}
-                                    width={100}
-                                />
-                            </div>
-                            <nav className="-mx-3 flex flex-1 justify-end">
+                        <main className="mb-10 flex grow flex-col items-center justify-center text-center">
+                            <h1 className="mb-6 text-7xl">
+                                WSC Tech<br></br>Management Portal
+                            </h1>
+                            <p className="mb-10">
+                                Temasek Polytechnic | School of Informatics & IT
+                            </p>
+                            <SecondaryButton disabled={false}>
                                 {auth.user ? (
                                     <Link
                                         href={route('dashboard')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        className="text-sm"
                                     >
                                         Dashboard
                                     </Link>
                                 ) : (
                                     <Link
                                         href={route('login')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        className="text-sm"
                                     >
                                         Log in
                                     </Link>
                                 )}
-                            </nav>
-                        </header>
-
-                        <main className="mb-10 flex grow flex-col items-center justify-center text-center">
-                            <h1 className="mb-6 text-7xl">
-                                WSC Tech<br></br>Management Portal
-                            </h1>
-                            <p>
-                                Temasek Polytechnic | School of Informatics & IT
-                            </p>
+                            </SecondaryButton>
                         </main>
                     </div>
                 </div>
