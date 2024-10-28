43fbb74db367cbe2b0a8cb77275c9ba047e9cd02
KohKeira <kohkeira996@gmail.com>
2024-10-28 10:16:05 +0800

    Add optional leading and trailing helper text to text input

--- resources/js/Components/TextInput.tsx
+++ resources/js/Components/TextInput.tsx
@@ -11,8 +11,15 @@ export default forwardRef(function TextInput(
         type = 'text',
         className = '',
         isFocused = false,
+        trailingHelperText = '',
+        leadingHelperText = '',
+
         ...props
-    }: InputHTMLAttributes<HTMLInputElement> & { isFocused?: boolean },
+    }: InputHTMLAttributes<HTMLInputElement> & {
+        isFocused?: boolean;
+        trailingHelperText?: string;
+        leadingHelperText?: string;
+    },
     ref,
 ) {
     const localRef = useRef<HTMLInputElement>(null);
@@ -28,14 +35,28 @@ export default forwardRef(function TextInput(
     }, [isFocused]);
 
     return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 ' +
-                className
-            }
-            ref={localRef}
-        />
+        <div className="mt-1 flex">
+            {leadingHelperText !== '' && (
+                <span className="inline-flex items-center rounded rounded-e-none border border-e-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+                    {leadingHelperText}
+                </span>
+            )}
+            <input
+                {...props}
+                ref={localRef}
+                type={type}
+                className={
+                    'block w-full min-w-0 flex-1 rounded border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 ' +
+                    className +
+                    (trailingHelperText !== '' ? ' rounded-e-none' : '') +
+                    (trailingHelperText !== '' ? ' rounded-s-none' : '')
+                }
+            />
+            {trailingHelperText !== '' && (
+                <span className="inline-flex items-center rounded rounded-s-none border border-s-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+                    {trailingHelperText}
+                </span>
+            )}
+        </div>
     );
 });
