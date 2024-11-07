4646b2ec00431f611e2d6a44ebb4858e2db4d637
Pure <111236541+PureStupid@users.noreply.github.com>
2024-11-07 23:04:33 +0800

    Add routes for managing training sessions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TrainingSessionController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\VerifyAdminRole;
 use Illuminate\Foundation\Application;
@@ -17,15 +18,18 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::middleware(VerifyAdminRole::class)->resource('users', UserController::class,['except'=>'index']);
+    Route::get('/training-sessions', [TrainingSessionController::class, 'index'])->name('training-sessions.index');
+    Route::get('/training-sessions/create', [TrainingSessionController::class, 'create'])->name('training-sessions.create');
+    Route::post('/training-sessions', [TrainingSessionController::class, 'store'])->name('training-sessions.store');
+
+    Route::middleware(VerifyAdminRole::class)->resource('users', UserController::class, ['except' => 'index']);
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
-
 });
 
 
